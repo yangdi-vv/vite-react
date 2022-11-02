21232cbb6235599cd75728a753135731a836e976
YD <di.yang@smartmore.com>
2022-11-02 15:06:37 +0800

    fix: test

--- src/views/pageA/pageA.tsx
+++ src/views/pageA/pageA.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+
 // Context 可以让我们无须明确地传遍每一个组件，就能将值深入传递进组件树。
 // 为当前的 theme 创建一个 context（“light”为默认值）。
 const ThemeContext = React.createContext('light')
+
 class App extends React.Component {
   render (): JSX.Element {
     // 使用一个 Provider 来将当前的 theme 传递给以下的组件树。
